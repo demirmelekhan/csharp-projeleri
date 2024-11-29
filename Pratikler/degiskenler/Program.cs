ab0b5f307d05abf2572a9fb52b33d2c86e45ac82
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2024-11-29 20:41:01 +0300

    Normal dosyalar eklendi

---
+++ Pratikler/degiskenler/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace degiskenler
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string str1="20";
+            int int1=20;
+            string yenideger = str1 + int1.ToString();
+            Console.WriteLine("sonuc=" + yenideger);
+            
+        }
+    }
+}
