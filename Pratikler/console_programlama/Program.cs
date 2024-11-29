3c94e8654372e97950e1a351c2d2a5014403fffe
Melek Demirhan <149537896+demirmelekhan@users.noreply.github.com>
2024-11-29 09:41:28 +0300

    Pratikler/degiskenler eklendi

---
+++ Pratikler/console_programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    namespace console_programlama
+    {
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Merhaba Dunya!");
+                Console.WriteLine("İsminizi girin:");
+                string? name = Console.ReadLine();
+                Console.WriteLine("Soyadınızı Girin:");
+                string? surname = Console.ReadLine();
+
+                Console.WriteLine("Merhaba " + name + " " + surname);
+
+            }
+        }
+    }
